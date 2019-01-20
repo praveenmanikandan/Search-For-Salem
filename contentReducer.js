e0931ba91cdb567371c7131160baee8500c11676
praveenmanikandan <45013819+praveenmanikandan@users.noreply.github.com>
2019-01-20 12:09:51 +0530

    Add files via upload

---
+++ contentReducer.js
@@ -0,0 +1,249 @@
+export default function() {
+  return {
+    Category: [
+      {
+        id: 1,
+        title: "Entertainment",
+        content: [
+          {
+            id: 1,
+            title: "entertainment one",
+            content: [{}]
+          },
+          {
+            id: 2,
+            title: "entertainment two",
+            content: [{}]
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: "Food",
+        content: [
+          {
+            id: 1,
+            title: "food one",
+            content: [{}]
+          },
+          { id: 2, title: "food 2", content: [{}] }
+        ]
+      },
+      {
+        id: 3,
+        title: "Eductaion",
+        content: [
+          {
+            id: 1,
+            title: "Education one",
+            content: [{}]
+          },
+          { id: 2, title: "Education 2", content: [{}] }
+        ]
+      },
+      {
+        id: 4,
+        title: "ATM",
+        content: [
+          {
+            id: 1,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "1/73 CUDDALORE MAIN ROAD, ATHUR TALUK, SALEM DIST, PUTHIRAGOUNDAMPALAYAM, PIN, Puthira Goundan Palayam, Salem - 636119",
+            area: "Puthira Goundan Palayam",
+            content: [{ title: "pincode", content: "636119" }]
+          },
+          {
+            id: 2,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "NO. 4/109/1, MAJRA KOLLAPATTI,STEEL PLANT MAIN ROAD, S.KOLLAPATTI (PO) SALEM - 636 030, S. Kollapatti, Salem - 636030",
+            area: "STEEL PLANT MAIN ROAD",
+            content: [{ title: "pincode", content: "636030" }]
+          },
+          {
+            id: 3,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "NO.1/1-35, OMALUR MAIN ROAD, THARAMANGALAM, SALEM- 636 502, S. Kollapatti, Salem - 636502",
+            area: "OMALUR MAIN ROAD",
+            content: [{ title: "pincode", content: "636502" }]
+          },
+          {
+            id: 4,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "S F NO 32/2 SUBBU COMPLEX, ATTUR MAIN ROAD, VAZHAPADI DIST : SALEM, Valapady, Salem - 636115",
+            area: "ATTUR MAIN ROAD, VAZHAPADI DIST",
+            content: [{ title: "pincode", content: "636115" }]
+          },
+          {
+            id: 5,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "BPCL OUTLET, AVM HI TECH FUEL PARK BANGALORE TO SALEM HIGHWAY PANNAPPATTI, OMALUR TK. SALEM DT. PIN: 636 305, Karuvalli, Salem - 636305",
+            area: "SALEM HIGHWAY PANNAPPATTI,",
+            content: [{ title: "pincode", content: "636305" }]
+          },
+          {
+            id: 6,
+            title: "Indian Overseas Bank ATM",
+            address:
+              "P S K COMPLEX, KARUVALLI POST OMALUR TALUK, CHINNA THIRUPATHI KARUVALLI DIST : SALEM, Karuvalli, Salem - 636305",
+            area: "CHINNA THIRUPATHI ",
+            content: [{ title: "pincode", content: "636305" }]
+          },
+          {
+            id: 7,
+            title: "Canara Bank ATM",
+            address: "POSTBOXNO.20CSICompound,Salem",
+            area: "CSICompound",
+            content: [{ title: "pincode", content: "" }]
+          },
+          {
+            id: 8,
+            title: "Canara Bank ATM",
+            address: "TOWNRAILWAYSTATION,  Salem",
+            area: "TOWNRAILWAYSTATION",
+            content: [{ title: "pincode", content: "" }]
+          },
+          {
+            id: 9,
+            title: "Karnataka Bank ATM",
+            address: "316 B,7TH Cross,Brindavan Road, Salem - 636004",
+            area: "Brindavan Road",
+            content: [{ title: "pincode", content: "636004" }]
+          },
+          {
+            id: 10,
+            title: "Dhanlaxmi Bank ATM",
+            address:
+              "SF No 2011 old T S No 96/2 Trichy Main Road,Salem - 636006",
+            area: "Trichy Main Road",
+            content: [{ title: "pincode", content: "636006" }]
+          },
+          {
+            id: 11,
+            title: "HDFC Bank ATM",
+            address:
+              "No.331, Chairman Ramalingam Street, Ammapet,Salem - 636003",
+            area: "Ammapet",
+            content: [{ title: "pincode", content: "636003" }]
+          },
+          {
+            id: 12,
+            title: "HDFC Bank ATM",
+            address: "73, Salem Main Road, Elampillai, Salem - 637502",
+            area: "Elampillai",
+            content: [{ title: "pincode", content: "637502" }]
+          },
+          {
+            id: 1,
+            title: "HDFC Bank ATM",
+            address:
+              "133 / 31 A Sethukrishna Trade Center, Trichy Main Road, Gugai, Salem - 636006",
+            area: "Trichy Main Road",
+            content: [{ title: "pincode", content: "636006" }]
+          },
+          {
+            id: 2,
+            title: "Indian Bank ATM",
+            address: "Dharmapuri Main Road,Chamrajpet, Salem - 636451",
+            area: "Chamrajpet",
+            content: [{ title: "pincode", content: "636451" }]
+          },
+          {
+            id: 3,
+            title: "Indian Bank ATM",
+            address: "8/12, M T C Road,Mettur Dam, Salem - 636401",
+            area: "Mettur Dam",
+            content: [{ title: "pincode", content: "636401" }]
+          },
+          {
+            id: 4,
+            title: "Indian Bank ATM",
+            address: "NEW BUS STAND, Salem - 636007",
+            area: "NEW BUS STAND",
+            content: [{ title: "pincode", content: "636007" }]
+          },
+          {
+            id: 5,
+            title: "Syndicate Bank ATM",
+            address: "446/530 ,Trichy Main Road,Gugai, Salem - 636006",
+            area: "Gugai",
+            content: [{ title: "pincode", content: "636006" }]
+          },
+          {
+            id: 6,
+            title: "Bank of Baroda ATM",
+            address:
+              "24, Officers Line, Opp. To Voorhees College, Vellore, Salem - 632001",
+            area: "Vellore",
+            content: [{ title: "pincode", content: "632001" }]
+          },
+          {
+            id: 7,
+            title: "Bank of Baroda ATM",
+            address:
+              "Rathinasamy Pillai Maligai,Door No 296, Omalur Road, Salem - 636009",
+            area: "Omalur Road",
+            content: [{ title: "pincode", content: "636009" }]
+          },
+          {
+            id: 8,
+            title: "United Commercial Bank ATM",
+            address: "CSI COMPOUND,FORT MAIN ROAD,Salem - 636001",
+            area: "CSI COMPOUND",
+            content: [{ title: "pincode", content: "636001" }]
+          },
+          {
+            id: 9,
+            title: "ING Vysya Bank ATM",
+            address:
+              "D.No. 171/172, Kamatchiammal Plaza, Cherry Road, Salem - 636007",
+            area: "Cherry Road",
+            content: [{ title: "pincode", content: "636007" }]
+          },
+          {
+            id: 10,
+            title: "Lakshmi Vilas Bank ATM",
+            address:
+              "Door No.252,M/s.Vijayasree Towers,Cherry Road,Kumarasamy Patty, Salem - 636007",
+            area: "Kumarasamy Patty",
+            content: [{ title: "pincode", content: "636007" }]
+          },
+          {
+            id: 11,
+            title: "State Bank of Hyderabad ATM",
+            address: "272, Cherry Road Head Post Office, Salem - 636001",
+            area: "Head Post Office",
+            content: [{ title: "pincode", content: "636001" }]
+          },
+          {
+            id: 12,
+            title: "Lakshmi Vilas Bank ATM",
+            address: "62/63,Bazaar street, Salem - 636001",
+            area: "Bazaar street",
+            content: [{ title: "pincode", content: "636001" }]
+          }
+        ]
+      },
+      {
+        id: 5,
+        title: "Ammusement Park",
+        content: [
+          {
+            id: 1,
+            title: "Amm.park one",
+            content: [{}]
+          },
+          {
+            id: 2,
+            title: "Amm.park two",
+            content: [{}]
+          }
+        ]
+      }
+    ]
+  };
+}
